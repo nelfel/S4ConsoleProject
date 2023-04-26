9e628c2e7a01e0214fc6d4238a131e0e60f5f190
Nelson Acosta <nelson.acosta.malpartida.com>
2023-04-25 21:24:15 -0500

    Agregar archivos de proyecto.

---
+++ S4ConsoleProject/Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S4ConsoleProject
+{
+
+    public class Persona
+    {
+        public int dni { get; set; }
+        public string nombreCompleto { get; set; }
+        public string telefono { get; set; }
+        public bool estado { get; set; }
+
+
+        public Persona(int dni, string nombreCompleto,string telefono, bool estado)
+        {
+            this.dni = dni;
+            this.nombreCompleto = nombreCompleto;
+            this.telefono = telefono;
+            this.estado = estado;
+        }
+
+        public Persona() { }
+
+    }
+    
+}
---
+++ S4ConsoleProject/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+using S4ConsoleProject;
+
+Console.WriteLine("Hello, World!");
+
+string curso = "Desarrollo de Ambiente web";
+int edad = 22;
+double salario = 2000;
+
+var alumno = "Luis Chang";
+var peso = 70;
+var notaFinal = 18.50;
+
+var persona1 = new Persona();
+persona1.dni = 9922882;
+persona1.telefono = "998399039";
+persona1.nombreCompleto = "Paolo Guerrero";
+persona1.estado = true;
+
+var persona2 = new Persona()
+{
+    dni = 99888833,
+    telefono = "998898939",
+    nombreCompleto = "Gianluca Lapadula",
+    estado = true
+};
+
+var persona3 = new Persona(18929211, "Claudio Pizarro", "323232323", false);
+
+var personas = new List<Persona>();
+personas.Add(persona1);
+personas.Add(persona2);
+personas.Add(persona3);
+
+// lambda Expressions
+var busqueda = personas.Where(x => x.dni == 9922882).FirstOrDefault();
+
+//LINQ Query Expressions
+
+
+if (busqueda != null)
+    Console.WriteLine("La persona encontrada es: " + busqueda.nombreCompleto);
+else
+    Console.WriteLine("La persona no existe");
+
+
+Console.ReadKey();
